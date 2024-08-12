3d2f6cfa52dd84d3c98683dd20119b0cf4dc5dda
amc510 <amc510@uclive.ac.nz>
2024-08-12 12:41:02 +1200

    Implemented the rest of the tests for DataTable

--- src/main/java/seng202/team0/database/Value.java
+++ src/main/java/seng202/team0/database/Value.java
@@ -1,5 +1,132 @@
 package seng202.team0.database;
 
-public class Value {
+/**
+ * This class represents a variant of possible DataTable values. Type erasure puts no bound on
+ * possible types and is a major source of bugs. We can solve this by enforcing a finite set.
+ * Unfortunately there does not appear to be an easy or efficient way of representing a union.
+ *
+ * @author Angus McDougall
+ */
+public abstract class Value {
+
+
+  /**
+   * Makes a string value.
+   *
+   * @param value Value
+   * @return String value
+   */
+  public static Value make(String value) {
+    return new StringValue(value);
+  }
+
+  /**
+   * Makes a real value.
+   *
+   * @param value Value
+   * @return Real value
+   */
+  public static Value make(double value) {
+    return new RealValue(value);
+  }
+
+  /**
+   * Checks if the contained value is a string.
+   *
+   * @return If the contained value is a string
+   */
+  public boolean isString() {
+    return this instanceof StringValue;
+  }
+
+  /**
+   * Checks if the contained value is a real.
+   *
+   * @return If the contained value is a real
+   */
+  public boolean isReal() {
+    return this instanceof RealValue;
+  }
+
+  /**
+   * Gets the type index of the contained value. This is deliberately opaque.
+   *
+   * @return Index of the type
+   */
+  public abstract int getTypeIndex();
+
+  /**
+   * Gets the contained value as a string.
+   *
+   * @return The current value as a string
+   * @throws ClassCastException If the contained value is not a string
+   */
+  public String getAsString() throws ClassCastException {
+    return ((StringValue) this).value;
+  }
+
+  /**
+   * Gets the contained value as a real.
+   *
+   * @return The current value as a real
+   * @throws ClassCastException If the contained value is not a real
+   */
+  public double getAsReal() throws ClassCastException {
+    return ((RealValue) this).value;
+  }
+
+  /**
+   * Type representing a string.
+   *
+   * @author Angus McDougall
+   */
+  private static class StringValue extends Value {
+
+    public String value;
+
+    StringValue(String value) {
+      this.value = value;
+    }
+
+    @Override
+    public int getTypeIndex() {
+      return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof StringValue value2) {
+        return value.equals(value2.value);
+      }
+      return false;
+    }
+  }
+
+  /**
+   * Type representing a real (currently double).
+   *
+   * @author Angus McDougall
+   */
+  private static class RealValue extends Value {
+
+    public double value;
+
+    RealValue(double value) {
+      this.value = value;
+    }
+
+    @Override
+    public int getTypeIndex() {
+      return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof RealValue value2) {
+        return value == value2.value;
+      }
+      return false;
+    }
+  }
 
 }
--- src/test/java/seng202/team0/unittests/database/DataTableTest.java
+++ src/test/java/seng202/team0/unittests/database/DataTableTest.java
@@ -1,8 +1,8 @@
 package seng202.team0.unittests.database;
 
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +10,18 @@ import org.junit.jupiter.api.Test;
 import seng202.team0.database.DataTable;
 import seng202.team0.database.Value;
 
+/**
+ * Tests DataTable.
+ *
+ * @author Angus McDougall
+ */
 public class DataTableTest {
 
   private DataTable table;
 
+  /**
+   * Constructs a table to use for testing.
+   */
   @BeforeEach
   public void setUp() {
     ArrayList<ArrayList<Value>> columns = new ArrayList<>();
@@ -21,7 +29,7 @@ public class DataTableTest {
     columns.add(new ArrayList<>());
     columns.add(new ArrayList<>());
     columns.get(0).add(Value.make(0.0));
-    columns.get(0).add(Value.make(-1.0));
+    columns.get(0).add(Value.make(1.0));
 
     columns.get(1).add(Value.make("fing"));
     columns.get(1).add(Value.make("ding"));
@@ -29,44 +37,103 @@ public class DataTableTest {
     columns.get(2).add(Value.make(0.0));
     columns.get(2).add(Value.make(-1.0));
 
-
     this.table = new DataTable(
         new String[]{"foo", "baz", "bar"},
         columns
     );
   }
 
+  /**
+   * Tests if the table fails to construct on invalid dimensions.
+   */
+  @Test
+  public void invalidTableDimensions() {
+    assertThrows(IllegalStateException.class, () -> {
+      ArrayList<ArrayList<Value>> columns = new ArrayList<>();
+      columns.add(new ArrayList<>());
+      columns.add(new ArrayList<>());
+      columns.get(0).add(Value.make(0.0));
+      columns.get(0).add(Value.make(1.0));
+
+      columns.get(1).add(Value.make("fing"));
+
+      new DataTable(new String[]{"foo", "bar"}, columns);
+    });
+  }
+
+  /**
+   * Tests if the tale fails to construct on invalid types.
+   */
+  @Test
+  public void invalidTableType() {
+    assertThrows(IllegalStateException.class, () -> {
+      ArrayList<ArrayList<Value>> columns = new ArrayList<>();
+      columns.add(new ArrayList<>());
+      columns.get(0).add(Value.make(0.0));
+      columns.get(0).add(Value.make("fing"));
+
+      new DataTable(new String[]{"foo"}, columns);
+    });
+  }
+
+
+  /**
+   * Tests columnSize.
+   */
   @Test
   public void columnSize() {
     assertEquals(table.columnSize(), 3);
   }
 
+  /**
+   * Tests rowSize.
+   */
   @Test
   void rowSize() {
     assertEquals(table.rowSize(), 2);
   }
 
+  /**
+   * Tests value access.
+   */
   @Test
   void getValue() {
+    assertEquals(table.get(1, 0), Value.make("fing"));
+
   }
 
-  @Test
-  void getColumnsType() {
-  }
 
+  /**
+   * Tests column naming
+   */
   @Test
   void getColumnName() {
+    assertEquals(table.getColumnName(0), "foo");
+    assertEquals(table.getColumnName(1), "baz");
+    assertEquals(table.getColumnName(2), "bar");
   }
 
+  /**
+   * Tests column iteration and order.
+   */
   @Test
   void getIterableForColumn() {
+    int idx = 0;
+    double[] expected = new double[]{0.0, 1.0};
+    for (Value value : table.getIterableForColumn(0)) {
+      assertEquals(value.getAsReal(), expected[idx++]);
+    }
   }
 
+  /**
+   * Tests getting a column index from name.
+   */
   @Test
-  void getColumnIndexFromString() {
+  void getColumnIndexFromName() {
+    assertEquals(table.getColumnIndexFromName("foo"), 0);
+    assertEquals(table.getColumnIndexFromName("baz"), 1);
+    assertEquals(table.getColumnIndexFromName("bar"), 2);
+    assertEquals(table.getColumnIndexFromName("dingus"), -1);
   }
 
-  @Test
-  void getRecordForIndex() {
-  }
 }
