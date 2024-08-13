474a266874d93e8c58db3c4c9ca3a21c5bfacab2
mpe133 <mpe133@uclive.ac.nz>
2024-08-13 21:07:35 +1200

    Added util package skeleton classes -Also added alcool package

---
+++ src/main/java/seng202/team0/util/PrimitiveView.java
@@ -0,0 +1,40 @@
+package seng202.team0.util;
+
+import seng202.team0.database.DataTable;
+import seng202.team0.database.Record;
+
+/**
+ * Primitive view (MORE DETAIL HERE!)
+ */
+public class PrimitiveView extends View {
+
+  /**
+   * Constructor (MORE DETAIL HERE!)
+   * @param table
+   */
+  public PrimitiveView(DataTable table) {
+    // TODO Implement me!
+  }
+
+  /**
+   * next (MORE DETAIL HERE!)
+   *
+   * @return
+   */
+  @Override
+  public Record next() {
+    // TODO Implement me!
+    return null;
+  }
+
+  /**
+   * Deep copy (MORE DETAIL HERE!)
+   *
+   * @return
+   */
+  @Override
+  public View deepCopy() {
+    // TODO Implement me!
+    return null;
+  }
+}
