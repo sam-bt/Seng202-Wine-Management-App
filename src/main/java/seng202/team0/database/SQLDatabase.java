d2f647153067b987a49ece179a75eb1af1741c4b
mpe133 <mpe133@uclive.ac.nz>
2024-08-16 21:21:10 +1200

    Tables can now be added to database - Tests now delete old db files - Database can also be disconnected - You can check if a table with a specific name exists in the database

--- src/main/java/seng202/team0/database/SQLDatabase.java
+++ src/main/java/seng202/team0/database/SQLDatabase.java
@@ -2,11 +2,15 @@ package seng202.team0.database;
 
 import com.sun.scenario.DelayedRunnable;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import seng202.team0.database.exceptions.DuplicateTableException;
 import seng202.team0.database.exceptions.TableNotFoundException;
 
@@ -20,11 +24,14 @@ public class SQLDatabase extends Database {
 
   // This is the path to the db file
   private final String dbPath = "jdbc:sqlite:sqlDatabase"+ File.separator + "SQLDatabase.db";
+
+  // Connection to the database
   private Connection connection = null;
 
   /**
    * Connects to a db file for management
    * The path to the file is specified by dbpath
+   *
    */
   public void setupDb() {
 
@@ -52,12 +59,45 @@ public class SQLDatabase extends Database {
   }
 
   /**
+   * Connects to the database, does not create db file!
+   *
+   * @throws FileNotFoundException if the db file doesn't exist
+   */
+  public void connectDb() throws FileNotFoundException {
+
+    // Check if the database is already connected
+    if (connection != null) {
+      System.out.println("Database already exists.");
+      return;
+    }
+
+    // Ensure database has already been made
+    if (!Files.exists(Paths.get(dbPath))) {
+      throw new FileNotFoundException(dbPath + " does not exist.");
+    }
+
+    try {
+      // Connect to database
+      connection = DriverManager.getConnection(dbPath);
+
+    } catch (SQLException e) {
+      // TODO More robust logging
+      e.printStackTrace();
+
+    }
+  }
+
+  /**
    * To disconnect/close database
+   *
    */
   public void disconnectDb() {
     if (connection != null) {
       try {
         this.connection.close();
+
+        // Reset connection
+        connection = null;
 
       } catch (SQLException e) {
         //TODO More robust logging here
@@ -75,7 +115,78 @@ public class SQLDatabase extends Database {
    */
   @Override
   public void addTable(String tableName, DataTable source) throws DuplicateTableException {
+    // Ensure the database is connected
+    if (connection == null) {
+      System.err.println("No database connection.");
+      return;
+    }
 
+    // Check if table exists
+    if (this.tableExists(tableName)) {
+      throw new DuplicateTableException(tableName);
+    }
+
+    // Construct prompt based on table
+    String statementString = constructTableStatementString(tableName, source);
+
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute(statementString);
+      System.out.println("Table " + tableName + " has been added.");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.err.println("Table " + tableName + " could not be added.");
+
+    }
+
+  }
+
+  /**
+   * Takes a table and constructs a SQL statement that can be used<br>
+   * to a table based of tableName parameters
+   *
+   * @param tableName name of the SQL table
+   * @param source DataTable that will be read from to construct statement
+   * @return the SQL statement itself
+   */
+  public String constructTableStatementString(String tableName, DataTable source) {
+
+    // Construct start of prompt
+    StringBuilder statement = new StringBuilder();
+    statement.append("CREATE TABLE IF NOT EXISTS "); // Create table
+    statement.append(tableName.replaceAll("\\s+", "")); // Using table name with no whitespace
+    statement.append(" ("); // Opening bracket for col names
+
+    int numOfCols = source.columnSize();
+
+    // Loop through cols and get names
+    for (int i = 0; i < numOfCols; i++) {
+
+      // Name of the col
+      statement.append(source.getColumnName(i));
+
+      // Check type of col
+      Value checkValue = source.get(i, 1);
+      if (checkValue.getTypeIndex() == 0) {
+        statement.append(" TEXT");
+      } else {
+        statement.append(" INTEGER");
+      }
+
+      // This assumes the primary key is in the first row
+      if (i == 0) {
+        statement.append(" PRIMARY KEY");
+      }
+
+      // Comma for last statement
+      if (i < numOfCols - 1) {
+        statement.append(",");
+      }
+    }
+
+    // Finish prompt and return
+    statement.append(")");
+    return statement.toString();
   }
 
   /**
@@ -97,6 +208,27 @@ public class SQLDatabase extends Database {
    */
   @Override
   public boolean tableExists(String tableName) {
+    try {
+
+      // Get database meta data
+      DatabaseMetaData metaData = connection.getMetaData();
+
+      // Strip tableName whitespace
+      tableName = tableName.replaceAll("\\s+", "");
+
+      // Query for table existence
+      ResultSet tables = metaData.getTables(null, null, tableName, null);
+
+      if (tables.next()) {
+        return true;
+      }
+
+      tables.close();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
     return false;
   }
 
--- src/test/java/seng202/team0/unittests/database/SQLDatabaseTest.java
+++ src/test/java/seng202/team0/unittests/database/SQLDatabaseTest.java
@@ -1,28 +1,37 @@
 package seng202.team0.unittests.database;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import seng202.team0.database.DataTable;
 import seng202.team0.database.SQLDatabase;
+import seng202.team0.database.Value;
+import seng202.team0.database.exceptions.DuplicateTableException;
 
 public class SQLDatabaseTest {
+  SQLDatabase db = new SQLDatabase();
 
   @BeforeEach
   public void setUp() {
-
+    try {
+      Files.deleteIfExists(Paths.get("sqlDatabase/SQLDatabase.db"));
+    } catch (IOException ignored) {
+    }
+    db.setupDb();
   }
 
   @Test
   public void testSetupDb() {
-    SQLDatabase db = new SQLDatabase();
-    db.setupDb();
 
     try {
       Assertions.assertTrue(db.getConnection().isValid(2));
 
     } catch (SQLException e) {
-      e.printStackTrace();
       Assertions.fail(); // Something has gone wrong so test should fail
 
     }
@@ -30,5 +39,41 @@ public class SQLDatabaseTest {
     db.disconnectDb();
 
     //TODO More testing needed here
+  }
+
+  @Test
+  public void testDisconnectDb() {
+    db.disconnectDb();
+    Assertions.assertNull(db.getConnection());
+  }
+
+  @Test
+  public void testAddTable() {
+    ArrayList<ArrayList<Value>> columns = new ArrayList<>();
+    columns.add(new ArrayList<>());
+    columns.add(new ArrayList<>());
+    columns.add(new ArrayList<>());
+    columns.get(0).add(Value.make(0.0));
+    columns.get(0).add(Value.make(1.0));
+
+    columns.get(1).add(Value.make("fing"));
+    columns.get(1).add(Value.make("ding"));
+
+    columns.get(2).add(Value.make(0.0));
+    columns.get(2).add(Value.make(-1.0));
+
+    DataTable testTable = new DataTable(
+        new String[]{"foo", "baz", "bar"},
+        columns
+    );
+
+    try {
+      db.addTable("test_table", testTable);
+
+    } catch (DuplicateTableException e) {
+      Assertions.fail();
+    }
+
+    Assertions.assertTrue(db.tableExists("test_table"));
   }
 }
