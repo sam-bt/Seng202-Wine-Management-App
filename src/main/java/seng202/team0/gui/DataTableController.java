125d3a76d1c27b286d1f402c512d25754f8aab9b
Sam Beattie <kiwisalmon2005@gmail.com>
2024-08-18 17:00:43 +1200

    added button that allows for the upload of a CSV file to the gui

--- src/main/java/seng202/team0/gui/DataTableController.java
+++ src/main/java/seng202/team0/gui/DataTableController.java
@@ -1,10 +1,16 @@
 package seng202.team0.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import seng202.team0.managers.ManagerContext;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+
 
 /**
  * Data Table Controller (MORE DETAIL HERE!)
+ * @author sbe119, amc510
  */
 public class DataTableController extends Controller{
 
@@ -16,6 +22,9 @@ public class DataTableController extends Controller{
   public DataTableController(ManagerContext managerContext) {
     super(managerContext);
   }
+
+  @FXML
+  private Button uploadCSVButton;
 
   /**
    * Launches the data set screen.
@@ -60,5 +69,27 @@ public class DataTableController extends Controller{
     managerContext.GUIManager.launchConsumptionCalculatorScreen(managerContext);
   }
 
+  /**
+   * Triggers the extension to upload a file when the upload csv button is pressed
+   */
+  public void uploadCSVFile() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open CSV File");
+    fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+
+    Stage stage = (Stage) uploadCSVButton.getScene().getWindow();
+    File selectedFile = fileChooser.showOpenDialog(stage);
+
+    if (selectedFile != null) {
+      processCSVFile(selectedFile);
+    }
+  }
+
+  /**
+   * Stand-in function to process csv files (will be replaced when functionality added)
+   */
+  public void processCSVFile(File file){
+    System.out.println("file processing");
+  }
 
 }
