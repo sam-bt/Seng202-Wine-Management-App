d76412dd276897b71671486b83f4671d79e4027e
Corey Hines <chi125@uclive.ac.nz>
2024-08-20 10:56:48 +1200

    fixed merge conflicts from a pervious commit

--- src/main/resources/map/leaflet_osm_map.js
+++ src/main/resources/map/leaflet_osm_map.js
@@ -6,14 +6,23 @@ const jsConnector = {
     initMap: initMap,
 };
 
-function initMap(format, data) {
+function initMap() {
     map = new L.map('map', MAP_CONFIG);
     new L.TileLayer(TILE_SERVER_URL, {
         attribution: TILE_SERVER_ATTRIBUTION
     }).addTo(map)
 
     // Marker Clusters - Combines nearby markers into 1
-    markersLayer = L.markerClusterGroup();
+    markersLayer = L.markerClusterGroup({
+        iconCreateFunction: function (cluster) {
+            return L.divIcon({
+                html: `<div><img src="wine_glass_small.png"><span>${cluster.getChildCount()}</span></div>`,
+                className: "custom-cluster-icon",
+                iconSize: L.point(36, 60),
+            });
+        },
+        // maxClusterRadius: 100,
+    });
     map.addLayer(markersLayer);
 
     // Heatmap
@@ -22,16 +31,28 @@ function initMap(format, data) {
 
     initTestData();
 
-    heatMapLayer.setData({data: heatMapData}, data);
+    heatMapLayer.setData({data: heatMapData}, TEST_DATA);
+}
 
-    findCoordinatesFromLocation("Christchurch");
+function addWineMarker(latitude, longitude, locationName = null) {
+    const marker = L.marker([latitude, longitude], {
+        icon: WINE_ICON,
+        riseOnHover: true,
+    });
+    if (locationName) {
+        marker.bindPopup(
+            `Name: WineName<br>
+            Type: WineType<br>
+            Location: ${locationName}`
+        );
+    }
+    markers.push(marker);
+    markersLayer.addLayer(marker);
+    heatMapData.push({ lat: latitude, lon: longitude });
 }
 
 function initTestData() {
-    TEST_DATA.forEach((location) => {
-        let marker = L.marker([location.lat, location.lon]);
-        markersLayer.addLayer(marker);
-
-        heatMapData.push({lat: location.lat, lon: location.lon})
-    })
+    TEST_DATA.forEach((point) => {
+        addWineMarker(point.lat, point.lon, point.name);
+    });
 }
