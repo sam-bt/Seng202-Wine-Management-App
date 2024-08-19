28852e5f103d7ffbe703790daee53672345c314b
amc510 <amc510@uclive.ac.nz>
2024-08-19 15:31:16 +1200

    Fixed compilation - WIP

--- src/main/java/seng202/team0/gui/DataTableController.java
+++ src/main/java/seng202/team0/gui/DataTableController.java
@@ -29,54 +29,6 @@ public class DataTableController extends Controller {
   private Button uploadCSVButton;
 
   /**
-   * Launches the data set screen.
-   */
-  @FXML
-  public void openDataSetsScreen() {
-    managerContext.GUIManager.launchDataSetsScreen(managerContext);
-  }
-
-  /**
-   * Launches the home screen.
-   */
-  @FXML
-  public void openHomeScreen() {
-    managerContext.GUIManager.launchHomeScreen(managerContext);
-  }
-
-  /**
-   * Launches the wine screen.
-   */
-  @FXML
-  public void openWineScreen() {
-    managerContext.GUIManager.launchWineScreen(managerContext);
-  }
-
-  /**
-   * Launches the list screen.
-   */
-  @FXML
-  public void openListScreen() {
-    managerContext.GUIManager.launchListsScreen(managerContext);
-  }
-
-  /**
-   * Launches the vineyard screen.
-   */
-  @FXML
-  public void openVineyardsScreen() {
-    managerContext.GUIManager.launchVineyardsScreen(managerContext);
-  }
-
-  /**
-   * Launches the consumption calculator screen.
-   */
-  @FXML
-  public void openConsumptionCalculatorScreen() {
-    managerContext.GUIManager.launchConsumptionCalculatorScreen(managerContext);
-  }
-
-  /**
    * Triggers the extension to upload a file when the upload csv button is pressed
    */
   public void uploadCSVFile() {
