e21a30cbedfaef7ac9ce361c2d349812d1dd0805
Corey Hines <chi125@uclive.ac.nz>
2024-08-20 10:44:37 +1200

    changed the geolocation resolver api to use the open street map api

--- src/main/resources/map/geolocation_resolver.js
+++ src/main/resources/map/geolocation_resolver.js
@@ -1,18 +1,38 @@
 // https://api-ninjas.com/api/geocoding
 // https://geocoding-api.open-meteo.com/v1/search?name=Christchurch
 
-const GEOLOCATION_URL = "https://geocoding-api.open-meteo.com/v1/search"
+const GEOLOCATION_URL = "https://nominatim.openstreetmap.org/search"
 
-function findCoordinatesFromLocation(location, callback) {
-    const request = new Request(GEOLOCATION_URL, {
-        method: "POST",
-        body: { name: location,
-            count: 1,
-            language: "en",
-            format: "json" },
-    });
-
-    fetch(request)
-        .then((response) => console.log(response))
-        .catch((err) => console.log(err));
+// https://nominatim.org/release-docs/develop/api/Search/
+// q - Free-form query string to search for
+function findLatitudeLongtitudeFromName(locationName) {
+    const url = "https://nominatim.openstreetmap.org/search";
+    const params = {
+        q: locationName,
+        format: "json",
+        limit: 1,
+    };
+    return axios
+        .get(url, {
+            params
+        })
+        .then((response) => {
+            const data = response.data;
+            if (data.length > 0) {
+                const {
+                    lat,
+                    lon
+                } = data[0];
+                return {
+                    latitude: parseFloat(lat),
+                    longitude: parseFloat(lon),
+                };
+            } else {
+                throw new Error("Location not found");
+            }
+        })
+        .catch((error) => {
+            console.error("Geocoding error:", error);
+            return null;
+        });
 }
