fed7421cf0d6efd807e5db2891e4cced947be1e1
mpe133 <mpe133@uclive.ac.nz>
2024-09-14 12:42:37 +1200

    Test for get wines in range with filters

--- src/test/java/seng202/team0/unittests/managers/DatabaseManagerTest.java
+++ src/test/java/seng202/team0/unittests/managers/DatabaseManagerTest.java
@@ -1,15 +1,18 @@
 package seng202.team0.unittests.managers;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seng202.team0.database.Wine;
 import seng202.team0.managers.DatabaseManager;
+import seng202.team0.util.Filters;
 
 class DatabaseManagerTest {
   DatabaseManager manager;
@@ -40,6 +43,138 @@ class DatabaseManagerTest {
   void getWinesInRange() throws SQLException {
     addWines(10);
     assertEquals(3, manager.getWinesInRange(1, 4).size());
+  }
+
+  @Test
+  void getWinesInRangeFiltered() throws SQLException {
+    addFilterableWines();
+
+    // Name filter
+    Filters filter = new Filters(
+        "wine",
+        "",
+        "",
+        0,
+        100,
+        0,
+        100,
+        0,
+        100
+    );
+
+    // Should return all wines
+    assertEquals(5, manager.getWinesInRange(0, 5, filter).size());
+
+    // Should all contain "wine"
+    for (Wine wine : manager.getWinesInRange(0, 4, filter)) {
+      Assertions.assertTrue(wine.getTitle().contains("wine"));
+    }
+
+    // Country filter
+    filter = new Filters(
+        "",
+        "us",
+        "",
+        0,
+        100,
+        0,
+        100,
+        0,
+        100
+    );
+
+    // Should only return 2 wines
+    assertEquals(2, manager.getWinesInRange(0, 5, filter).size());
+
+    // Should all have country as "us"
+    for (Wine wine : manager.getWinesInRange(0, 5, filter)) {
+      assertEquals("us", wine.getCountry());
+    }
+
+    // Winery filter
+    filter = new Filters(
+        "",
+        "",
+        "bob's wine",
+        0,
+        100,
+        0,
+        100,
+        0,
+        100
+    );
+
+    // Should only return 1 wine
+    assertEquals(1, manager.getWinesInRange(0, 5, filter).size());
+
+    // Should all have winery as "bob's wine"
+    for (Wine wine : manager.getWinesInRange(0, 5, filter)) {
+      assertEquals("bob's wine", wine.getWinery());
+    }
+
+    // Score filter
+    filter = new Filters(
+        "",
+        "",
+        "",
+        0,
+        90,
+        0,
+        100,
+        0,
+        100
+    );
+
+    // Should return 4 wines
+    assertEquals(4, manager.getWinesInRange(0, 5, filter).size());
+
+    // Should have a score between 0 and 90
+    for (Wine wine : manager.getWinesInRange(0, 5, filter)) {
+      Assertions.assertTrue(wine.getScorePercent() >= 0 && wine.getScorePercent() <= 90);
+    }
+
+    // Abv filter
+    filter = new Filters(
+        "",
+        "",
+        "",
+        0,
+        100,
+        21,
+        100,
+        0,
+        100
+    );
+
+    // Should return 3 wines
+    assertEquals(3, manager.getWinesInRange(0, 5, filter).size());
+
+    // Abv should be between 21 and 100
+    for (Wine wine : manager.getWinesInRange(0, 5, filter)) {
+      Assertions.assertTrue(wine.getAbv() >= 21 && wine.getAbv() <= 100);
+    }
+
+    // Price filter
+    filter = new Filters(
+        "",
+        "",
+        "",
+        0,
+        100,
+        0,
+        100,
+        40,
+        100
+    );
+
+    // Should get two wines
+    assertEquals(2, manager.getWinesInRange(0, 5, filter).size());
+
+    // Price should be between 40 and 100
+    for (Wine wine : manager.getWinesInRange(0, 5, filter)) {
+      Assertions.assertTrue(wine.getPrice() >= 40 && wine.getPrice() <= 100);
+    }
+
   }
 
   /**
@@ -80,5 +215,20 @@ class DatabaseManagerTest {
     manager.addWines(wines);
 
 
+  }
+
+  /**
+   * Adds a selection of preset wines to be used for testing filters
+   *
+   * @throws SQLException if adding wines fails
+   */
+  void addFilterableWines() throws SQLException {
+    ArrayList<Wine> wines = new ArrayList<>();
+    wines.add(new Wine("wine", "blue", "nz", "bob's wine", "na", 99, 25f, 10f));
+    wines.add(new Wine("Big wine", "green", "us", "joes's wine", "na", 65, 20f, 20f));
+    wines.add(new Wine("Funny wine", "blue", "us", "joes's wine", "na", 85, 24f, 50f));
+    wines.add(new Wine("Small wine", "red", "nz", "jill's wine", "na", 88, 18f, 25f ));
+    wines.add(new Wine("Cool wine", "green", "nz", "jill's wine", "na", 90, 23f, 40f));
+    manager.addWines(wines);
   }
 }
