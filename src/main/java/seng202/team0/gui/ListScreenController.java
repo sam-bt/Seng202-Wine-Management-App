cefde762bdf941ae692f3ddf8388745bc6cacc94
jmi157 <jmi157@uclive.ac.nz>
2024-09-15 17:38:26 +1200

    made it so you can change change between viewing lists and creating new lists by deleting and adding tabs to the tab pane.

--- src/main/java/seng202/team0/gui/ListScreenController.java
+++ src/main/java/seng202/team0/gui/ListScreenController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import seng202.team0.managers.ManagerContext;
 
 /**
@@ -12,10 +14,20 @@ import seng202.team0.managers.ManagerContext;
 public class ListScreenController extends Controller {
 
   @FXML
-  public Button pressButton;
+  public Button createListRequestButton;
+  @FXML
+  public Button backButton;
 
   @FXML
   public Label changeThisPlease;
+
+  @FXML
+  public TabPane listScreenTabs;
+
+  @FXML
+  public Tab tabViewing;
+  @FXML
+  public Tab tabCreating;
 
   /**
    * Constructor
@@ -26,8 +38,22 @@ public class ListScreenController extends Controller {
     super(managerContext);
   }
 
+  public void initialize() {
+    listScreenTabs.getTabs().remove(tabCreating);
+  }
 
-  public void onButtonPressed(ActionEvent actionEvent) {
-    changeThisPlease.setText("Wooo it worked!");
+
+  @FXML
+  public void onCreateListRequestButton(ActionEvent actionEvent) {
+    listScreenTabs.getTabs().add(tabCreating);
+    listScreenTabs.getTabs().remove(tabViewing);
+    createListRequestButton.setDisable(true);
+  }
+
+  @FXML
+  public void onBackButton(ActionEvent actionEvent) {
+    listScreenTabs.getTabs().add(tabViewing);
+    listScreenTabs.getTabs().remove(tabCreating);
+    createListRequestButton.setDisable(false);
   }
 }
