805a97faabcbba6d9ef4a22522f008554781ae4f
jmi157 <jmi157@uclive.ac.nz>
2024-09-15 19:16:30 +1200

    Lets the user create an arraylist of 5 names for wine lists, needs to be adjusted to be stored by user.

--- src/main/java/seng202/team0/gui/ListScreenController.java
+++ src/main/java/seng202/team0/gui/ListScreenController.java
@@ -2,11 +2,12 @@ package seng202.team0.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import seng202.team0.managers.ManagerContext;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * List Screen Controller (MORE DETAIL HERE!)
@@ -18,13 +19,21 @@ public class ListScreenController extends Controller {
   @FXML
   public Button backButton;
   @FXML
-  public Label changeThisPlease;
-  @FXML
   public TabPane listScreenTabs;
   @FXML
   public Tab tabViewing;
   @FXML
   public Tab tabCreating;
+
+  @FXML
+  public TextField listName;
+  @FXML
+  public Label errorText;
+
+  @FXML
+  public Button listOneButton, listTwoButton, listThreeButton, listFourButton, listFiveButton;
+
+  public ArrayList<String> wineLists = new ArrayList<String>();
 
   /**
    * Constructor
@@ -62,5 +71,42 @@ public class ListScreenController extends Controller {
     listScreenTabs.getTabs().add(tabViewing);
     listScreenTabs.getTabs().remove(tabCreating);
     createListRequestButton.setDisable(false);
+    if (wineLists.size() == 5) {
+      createListRequestButton.setDisable(true);
+    }
+    listName.setText("");
   }
+
+  @FXML
+  public void onCreateListConfirmButton(ActionEvent actionEvent) {
+    String name = listName.getText();
+    if (wineLists.contains(name)) {
+      errorText.setVisible(true);
+    } else {
+      errorText.setVisible(false);
+      wineLists.add(name);
+
+      listName.setText("");
+
+      if (!wineLists.isEmpty()) {
+        listOneButton.setText(wineLists.getFirst());
+        if (wineLists.size()>= 2) {
+          listTwoButton.setText(wineLists.get(1));
+          if (wineLists.size()>= 3) {
+            listThreeButton.setText(wineLists.get(2));
+            if (wineLists.size()>= 4) {
+              listFourButton.setText(wineLists.get(3));
+              if (wineLists.size() == 5) {
+                listFiveButton.setText(wineLists.get(4));
+              }
+            }
+          }
+        }
+      }
+    onBackButton(actionEvent);
+
+    }
+  }
+
+
 }
