224815c15ae3217dba13038339f2b529f6702517
Corey Hines <chi125@uclive.ac.nz>
2024-09-16 19:45:22 +1200

    added javadocs for GeoLocation

--- src/main/java/seng202/team0/database/GeoLocation.java
+++ src/main/java/seng202/team0/database/GeoLocation.java
@@ -1,17 +1,43 @@
 package seng202.team0.database;
 
+/**
+ * Represents a geographical location with latitude and longitude coordinates
+ */
 public class GeoLocation {
+
+  /**
+   * The latitude coordinate of this location
+   */
   private final double latitude;
+  /**
+   * The longitude coordinate of this location
+   */
   private final double longitude;
 
+  /**
+   * Constructs a new GeoLocation object with the specified latitude and longitude
+   * @param latitude
+   * @param longitude
+   */
   public GeoLocation(double latitude, double longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
   }
 
+  /**
+   * Returns the latitude coordinate of this location
+   *
+   * @return the latitude
+   */
   public double getLatitude() {
     return latitude;
   }
+
+  /**
+   * Returns the longitude of this location
+   *
+   * @return the longitude
+   */
   public double getLongitude() {
     return longitude;
   }
