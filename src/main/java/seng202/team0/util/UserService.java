f7f5515a263134656b36f3304b1f0d1d8badad89
Corey Hines <chi125@uclive.ac.nz>
2024-09-16 20:23:44 +1200

    cleaned up the user service class

--- src/main/java/seng202/team0/util/UserService.java
+++ src/main/java/seng202/team0/util/UserService.java
@@ -1,77 +1,89 @@
 package seng202.team0.util;
 
-import static java.util.Objects.isNull;
-
-import java.util.Objects;
 import seng202.team0.database.User;
 import seng202.team0.managers.ManagerContext;
 
 public class UserService {
-  public static String validateLogin(String username,String password, ManagerContext manager) {
-    if (Objects.equals(username, "") || Objects.equals(password, "")) {
+
+  public static String validateLogin(String username, String password, ManagerContext manager) {
+    if (username.isEmpty() || password.isEmpty()) {
       return "All fields must be filled!";
     }
-    seng202.team0.database.User userInfo = manager.databaseManager.getUser(username);
-    if (isNull(userInfo)) {
+
+    User userInfo = manager.databaseManager.getUser(username);
+    if (userInfo == null) {
       return "Username or Password is Incorrect";
-      } else if (Password.verifyPassword(password,userInfo.getPassword(), userInfo.getSalt()) && username.equals("admin") && Objects.equals(password, "admin")) {
-        return "Admin First Success";
-      } else if (Password.verifyPassword(password,userInfo.getPassword(), userInfo.getSalt()) && username.equals("admin")) {
-        return "Admin Success";
-      } else if (Password.verifyPassword(password,userInfo.getPassword(), userInfo.getSalt())) {
-        return "Success";
-      } else {
-        return "Username or Password is Incorrect";
     }
+
+    boolean validPassword = Password.verifyPassword(password, userInfo.getPassword(),
+        userInfo.getSalt());
+    if (!validPassword) {
+      return "Username or Password is Incorrect";
+    }
+
+    if (username.equals("admin") && password.equals("admin")) {
+      return "Admin First Success";
+    }
+    if (username.equals("admin")) {
+      return "Admin Success";
+    }
+    return "Success";
   }
-  public static String validateRegistration(String username,String password,String confirmPassword, ManagerContext manager) {
-    if (Objects.equals(username, "") || Objects.equals(password, "") || Objects.equals(confirmPassword, "")) {
+
+  public static String validateRegistration(String username, String password,
+      String confirmPassword, ManagerContext manager) {
+    if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
       return "All fields must be filled!";
     }
-    if (!Objects.equals(password, confirmPassword)) {
+    if (!password.equals(confirmPassword)) {
       return "Passwords do not match!";
     }
     if (username.length() < 3 || username.length() > 15 || !username.matches("[a-zA-Z0-9_]+")) {
       return "Invalid name, Please make sure that your name is between 3 and 15 characters long and only contains letters, numbers or underscores!";
-    } else if (password.length() < 3 || password.length() > 15 || !password.matches("[a-zA-Z0-9]+")) {
-      return "Invalid password, Please make sure that your password is between 3 and 15 characters long and only contains letters or numbers!";
-    } else {
-      String salt = Password.generateSalt();
-      String hashedPassword = Password.hashPassword(password,salt);
-      boolean userAdded = manager.databaseManager.addUser(username,hashedPassword, salt);
-      if (userAdded) {
-        return "Success";
-      } else {
-        return "Unexpected Error occured";
-      }
     }
+    if (password.length() < 3 || password.length() > 15 || !password.matches(
+        "[a-zA-Z0-9]+")) {
+      return "Invalid password, Please make sure that your password is between 3 and 15 characters long and only contains letters or numbers!";
+    }
+    String salt = Password.generateSalt();
+    String hashedPassword = Password.hashPassword(password, salt);
+    boolean userAdded = manager.databaseManager.addUser(username, hashedPassword, salt);
+    if (userAdded) {
+      return "Success";
+    }
+    return "Unexpected Error occured";
   }
 
-  public static String validateUpdate(String username, String oldPassword, String newPassword, ManagerContext manager) {
+  public static String validateUpdate(String username, String oldPassword, String newPassword,
+      ManagerContext manager) {
     User user = manager.databaseManager.getUser(username);
-    if (Objects.equals(oldPassword, "") || Objects.equals(newPassword, "")) {
+    if (oldPassword.isEmpty() || newPassword.isEmpty()) {
       return "All fields must be filled!";
     }
-    if (!Password.verifyPassword(oldPassword, user.getPassword(), user.getSalt())) {
-      return "Password incorrect";
+
+    boolean validPassword = Password.verifyPassword(oldPassword, user.getPassword(),
+        user.getSalt());
+    if (!validPassword) {
+      return "Password is Incorrect";
     }
-    if (Objects.equals(username, "admin") && Objects.equals(newPassword, "admin")) {
+    if (username.equals("admin") && newPassword.equals("admin")) {
       return "Admin password cannot be 'admin'";
     }
-    if (Objects.equals(oldPassword, newPassword)) {
+    if (oldPassword.equals(newPassword)) {
       return "New password cannot be same as old password";
     }
-    else if (newPassword.length() < 3 || newPassword.length() > 15 || !newPassword.matches("[a-zA-Z0-9]+")) {
+    if (newPassword.length() < 3 || newPassword.length() > 15 || !newPassword.matches(
+        "[a-zA-Z0-9]+")) {
       return "Invalid password, Please make sure that your password is between 3 and 15 characters long and only contains letters or numbers!";
     }
 
     String salt = Password.generateSalt();
-    String hashedPassword = Password.hashPassword(newPassword,salt);
-    boolean passwordUpdated = manager.databaseManager.updatePassword(username,hashedPassword, salt);
+    String hashedPassword = Password.hashPassword(newPassword, salt);
+    boolean passwordUpdated = manager.databaseManager.updatePassword(username, hashedPassword,
+        salt);
     if (passwordUpdated) {
       return "Success";
-    } else {
-      return  "Unexpected error Occured";
     }
+    return "Unexpected error Occurred";
   }
 }
