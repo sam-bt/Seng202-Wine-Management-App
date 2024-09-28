9a864e3f65030bb5ab91fb4bceeb7654c72d4b10
mru50 <mru50@uclive.ac.nz>
2024-09-28 19:32:09 +1200

    Quick refactor to break up the RegexProcessor tests

--- src/test/java/seng202/team6/util/RegexProcessorTest.java
+++ src/test/java/seng202/team6/util/RegexProcessorTest.java
@@ -6,29 +6,54 @@ import org.junit.jupiter.api.Test;
 
 public class RegexProcessorTest {
     RegexProcessor regexProcessor = new RegexProcessor();
+    String twentyfirst = "Pinot Noir made in 2005";
+    String twentieth = "Red made in 1984";
+    String nineteenth = "White made in 1896";
+    String noYear = "Rose of unknown origin";
+    String nonYearNUmber = "Rose number 1178";
 
+    /**
+     * Test extracting a 21st-century year from a string
+     */
     @Test
-    public void testExtractYear() {
-        String twentyfirst = "Pinot Noir made in 2005";
-        String twentieth = "Red made in 1984";
-        String nineteenth = "White made in 1896";
-        String noYear = "Rose of unknown origin";
-        String nonYearNUmber = "Rose number 1178";
+    public void testExtract21stCentury() {
+        String year = regexProcessor.extractYearFromString(twentyfirst);
+        assertEquals(year, "2005");
+    }
 
-        String output = regexProcessor.extractYearFromString(twentyfirst);
-        assertEquals(output, "2005");
+    /**
+     * Test extracting a 20th-century year from a string
+     */
+    @Test
+    public void testExtract20thCentury() {
+        String year = regexProcessor.extractYearFromString(twentieth);
+        assertEquals(year, "1984");
+    }
 
-        output = regexProcessor.extractYearFromString(twentieth);
-        assertEquals(output, "1984");
+    /**
+     * Test extracting a 19th-century year from a string
+     */
+    @Test
+    public void testExtract19thCentury() {
+        String year = regexProcessor.extractYearFromString(nineteenth);
+        assertEquals(year, "1896");
+    }
 
-        output = regexProcessor.extractYearFromString(nineteenth);
-        assertEquals(output, "1896");
+    /**
+     * Test processing a string with no numbers in it
+     */
+    @Test
+    public void testNoYearInString() {
+        String year = regexProcessor.extractYearFromString(noYear);
+        assertEquals(year, "-1");
+    }
 
-        output = regexProcessor.extractYearFromString(noYear);
-        assertEquals(output, "-1");
-
-        output = regexProcessor.extractYearFromString(nonYearNUmber);
-        assertEquals(output, "-1");
-
+    /**
+     * Test processing of an invalid year / number which doesn't meet the year requirements (1800-2099)
+     */
+    @Test
+    public void testNonYearNumberInString() {
+        String year = regexProcessor.extractYearFromString(nonYearNUmber);
+        assertEquals(year, "-1");
     }
 }
