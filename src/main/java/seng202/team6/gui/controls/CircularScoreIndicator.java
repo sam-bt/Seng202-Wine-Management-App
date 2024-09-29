f745538b70367f8f522d32b99598348ce38a34f0
Corey Hines <chi125@uclive.ac.nz>
2024-09-29 13:00:56 +1300

    added an indicator to the score control to show the score is out of 100

--- src/main/java/seng202/team6/gui/controls/CircularScoreIndicator.java
+++ src/main/java/seng202/team6/gui/controls/CircularScoreIndicator.java
@@ -2,9 +2,12 @@ package seng202.team6.gui.controls;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Pos;
 import javafx.scene.control.Control;
 import javafx.scene.control.SkinBase;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -32,6 +35,7 @@ public class CircularScoreIndicator extends Control {
   private static class CircularWineScoreIndicatorSkin extends SkinBase<CircularScoreIndicator> {
     private final Pane pane;
     private final Circle background;
+    private final VBox scoreTextWrapper;
     private final Text scoreText;
 
     public CircularWineScoreIndicatorSkin(CircularScoreIndicator control) {
@@ -48,9 +52,15 @@ public class CircularScoreIndicator extends Control {
 
       scoreText = new Text();
       scoreText.setFill(Color.BLACK);
-      scoreText.setFont(Font.font(16));
+      scoreText.setFont(Font.font(28));
 
-      pane.getChildren().addAll(background, scoreText);
+      Text scoreMax = new Text();
+      scoreMax.setText("/100");
+      scoreMax.setFont(Font.font(18));
+      scoreTextWrapper = new VBox(scoreText, scoreMax);
+      scoreTextWrapper.setAlignment(Pos.CENTER);
+
+      pane.getChildren().addAll(background, scoreTextWrapper);
       getChildren().add(pane);
 
       control.scoreProperty().addListener((obs, oldVal, newVal) -> updateScore(newVal.doubleValue()));
@@ -65,6 +75,8 @@ public class CircularScoreIndicator extends Control {
       double size = Math.min(getSkinnable().getWidth(), getSkinnable().getHeight());
       pane.setPrefSize(size, size);
 
+      scoreTextWrapper.setPrefSize(size, size);
+
       double centerX = size / 2;
       double centerY = size / 2;
       double radius = size / 2 - 5; // 5 pixels padding
@@ -78,14 +90,14 @@ public class CircularScoreIndicator extends Control {
     }
 
     private void updateScore(double score) {
-      scoreText.setText(String.format("%.1f", score)); // only to 1 decimal place
+      scoreText.setText(String.format("%.0f", score)); // don't show decimal place of score
 
       // calculate the center (x, y coordinate) of the circle which is half the diameter
-      double size = Math.min(getSkinnable().getWidth(), getSkinnable().getHeight());
-      double centerX = size / 2;
-      double centerY = size / 2;
-      scoreText.setX(centerX - scoreText.getBoundsInLocal().getWidth() / 2);
-      scoreText.setY(centerY + scoreText.getBoundsInLocal().getHeight() / 4);
+//      double size = Math.min(getSkinnable().getWidth(), getSkinnable().getHeight());
+//      double centerX = size / 2;
+//      double centerY = size / 2;
+//      scoreTextWrapper.setLayoutX(centerX - scoreText.getBoundsInLocal().getWidth() / 2);
+//      scoreTextWrapper.setLayoutY(centerY + scoreText.getBoundsInLocal().getHeight() / 4);
     }
   }
 }
