784b6e397a5ce9dd4b1a4c89bec7fa0b0ac5b051
Corey Hines <chi125@uclive.ac.nz>
2024-09-30 15:29:41 +1300

    added sample of data when matching columns in the wine importer

--- src/main/java/seng202/team6/gui/AdminController.java
+++ src/main/java/seng202/team6/gui/AdminController.java
@@ -1,17 +1,25 @@
 package seng202.team6.gui;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -46,14 +54,9 @@ public class AdminController extends Controller {
 
     dataColumnsContainer.getRowConstraints().clear();
     dataColumnsContainer.setHgap(10);
-    dataColumnsContainer.setVgap(10);
+    dataColumnsContainer.setVgap(20);
 
-    makeColumnRemapList(new String[]{"Name", "Country", "Region", "Colour", "Test"}, List.of());
-
-//    dataColumnsContainer.add(createSomeElement("Title"), 0, 0);
-//    dataColumnsContainer.add(createSomeElement("Country"), 1, 0);
-//    dataColumnsContainer.add(createSomeElement("Colour"), 2, 0);
-
+//    makeColumnRemapList(new String[]{"Name", "Country", "Region", "Colour", "Test"}, List.of());
   }
 
   @FXML
@@ -96,7 +99,7 @@ public class AdminController extends Controller {
   }
 
   private void makeColumnRemapList(String[] columnNames, List<String[]> rows) {
-    int columns = 3;
+    int columns = 4;
     int row = 0;
     for (int i = 0; i < columnNames.length; i++) {
       String columnName = columnNames[i];
@@ -106,8 +109,13 @@ public class AdminController extends Controller {
         row++;
       }
 
+      ObservableList<String> sampleValues = FXCollections.observableArrayList();
+      for (int j = 0; j < 5 && j != rows.size(); j++) {
+        sampleValues.add(rows.get(j)[i]);
+      }
+
       WinePropertyName possiblePropertyName = WinePropertyName.tryMatch(columnName);
-      GridPane wrapper = createSomeElement(columnName, possiblePropertyName);
+      GridPane wrapper = createSomeElement(columnName, possiblePropertyName, sampleValues);
       dataColumnsContainer.add(wrapper, column, row);
     }
   }
@@ -119,21 +127,23 @@ public class AdminController extends Controller {
   }
 
   // idk what to call this
-  private GridPane createSomeElement(String columnName, WinePropertyName possiblePropertyName) {
+  private GridPane createSomeElement(String columnName, WinePropertyName possiblePropertyName,
+      ObservableList<String> sampleValues) {
     GridPane gridPane = new GridPane();
     RowConstraints firstRow = new RowConstraints();
     RowConstraints secondRow = new RowConstraints();
+    RowConstraints thirdRow = new RowConstraints();
     ColumnConstraints firstColumn = new ColumnConstraints();
     ColumnConstraints secondColumn = new ColumnConstraints();
-    gridPane.getRowConstraints().addAll(firstRow, secondRow);
+    gridPane.getRowConstraints().addAll(firstRow, secondRow, thirdRow);
     gridPane.getColumnConstraints().addAll(firstColumn, secondColumn);
     gridPane.setHgap(10);
     gridPane.setVgap(10);
     gridPane.setPadding(new Insets(10));
     gridPane.setStyle("-fx-background-color: #f3f4f6; -fx-background-radius: 10px;");
 
-    Label columnNameInFile = new Label("Column Name in File: " + columnName);
-    columnNameInFile.setFont(Font.font(16));
+    Label columnNameInFile = new Label("Column: " + columnName);
+    columnNameInFile.setStyle("-fx-font-weight: bold; -fx-font-size: 16;");
     gridPane.add(columnNameInFile, 0, 0, 2, 1);
     GridPane.setHalignment(columnNameInFile, HPos.CENTER);
 
@@ -146,10 +156,41 @@ public class AdminController extends Controller {
       choiceBox.getSelectionModel().select(possiblePropertyName);
     }
 
+    Label valuesLabel = new Label("Values from this column");
+    valuesLabel.setFont(Font.font(16));
+
+    ListView<String> valuesList = new ListView<>(sampleValues);
+    valuesList.setMinHeight(200);
+    valuesList.setMaxHeight(200);
+    valuesList.setPrefHeight(200);
+    valuesList.setCellFactory(param -> new ListCell<>() {
+      @Override
+      protected void updateItem(String text, boolean empty) {
+        super.updateItem(text, empty); // Make sure to call super
+        if (empty || text == null || text.isEmpty()) {
+          setText(null);
+        } else {
+          maxWidthProperty().bind(param.widthProperty().subtract(20));
+          minWidthProperty().bind(param.widthProperty().subtract(20));
+          prefWidthProperty().bind(param.widthProperty().subtract(20));
+          setWrapText(true);
+          setText(text);
+        }
+      }
+    });
+
+    VBox valuesWrapper = new VBox();
+    valuesWrapper.setPadding(new Insets(10, 0, 0, 0));
+    valuesWrapper.setAlignment(Pos.CENTER);
+    valuesWrapper.getChildren().addAll(valuesLabel, valuesList);
+    gridPane.add(valuesWrapper, 0, 2, 2, 1);
+    GridPane.setHalignment(valuesWrapper, HPos.CENTER);
+
     firstColumn.setHgrow(Priority.ALWAYS);
     secondColumn.setHgrow(Priority.ALWAYS);
     firstColumn.setPercentWidth(50);
     secondColumn.setPercentWidth(50);
+
 
     GridPane.setFillWidth(gridPane, true);
     return gridPane;
