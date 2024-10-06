1db27e344a04551dc7bc390d96af7d40f96df764
chi125 <chi125@uclive.ac.nz>
2024-10-06 15:40:29 +1300

    created tour planning service to store what vineyards are currently in a tour

---
+++ src/main/java/seng202/team6/service/TourPlanningService.java
@@ -0,0 +1,37 @@
+package seng202.team6.service;
+
+import seng202.team6.managers.DatabaseManager;
+import seng202.team6.model.Vineyard;
+import seng202.team6.model.VineyardTour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TourPlanningService {
+    private final DatabaseManager databaseManager;
+    private final VineyardTour vineyardTour;
+    private final List<Vineyard> vineyards = new ArrayList<>();
+
+    public TourPlanningService(DatabaseManager databaseManager, VineyardTour vineyardTour) {
+        this.databaseManager = databaseManager;
+        this.vineyardTour = vineyardTour;
+    }
+
+    public VineyardTour getVineyardTour() {
+        return vineyardTour;
+    }
+
+    public List<Vineyard> getVineyards() {
+        return vineyards;
+    }
+
+    public void addVineyard(Vineyard vineyard) {
+        databaseManager.getVineyardTourDAO().addVineyard(vineyardTour, vineyard);
+        vineyards.add(vineyard);
+    }
+
+    public void removeVineyard(Vineyard vineyard) {
+        databaseManager.getVineyardTourDAO().removeVineyard(vineyardTour, vineyard);
+        vineyards.remove(vineyard);
+    }
+}
