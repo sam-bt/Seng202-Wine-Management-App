200fffc28c241be55d89efb833b6bb696a54eb74
Sam Beattie <sbe119@uclive.ac.nz>
2024-10-08 20:53:44 +1300

    checking corey branch

--- src/test/java/seng202/team6/unittests/dao/VineyardDaoTest.java
+++ src/test/java/seng202/team6/unittests/dao/VineyardDaoTest.java
@@ -36,9 +36,12 @@ public class VineyardDaoTest {
     vineyardDao = databaseManager.getVineyardsDao();
     vineyardDao.setUseCache(false);
 
-    Vineyard testVineyard1 = new Vineyard(1,"Test1 Vineyard", "9 Maidstone Road", "Tasman", "www.test.com", "test", "www.test.com", new GeoLocation(1,1));
-    Vineyard testVineyard2 = new Vineyard(2,"Test2 Vineyard", "111 Test Road", "Taranaki", "www.fake.com", "oops", "www.dog.com", new GeoLocation(100,100));
-    Vineyard testVineyard3 = new Vineyard(2,"Test3 Vineyard", "999 Bill Street", "Northland", "www.lol.com", "cat", "www.uno.com", new GeoLocation(-134.643,159.09));
+    Vineyard testVineyard1 = new Vineyard(1, "Test1 Vineyard", "9 Maidstone Road", "Tasman",
+        "www.test.com", "test", "www.test.com", new GeoLocation(1, 1));
+    Vineyard testVineyard2 = new Vineyard(2, "Test2 Vineyard", "111 Test Road", "Taranaki",
+        "www.fake.com", "oops", "www.dog.com", new GeoLocation(100, 100));
+    Vineyard testVineyard3 = new Vineyard(2, "Test3 Vineyard", "999 Bill Street", "Northland",
+        "www.lol.com", "cat", "www.uno.com", new GeoLocation(-134.643, 159.09));
     vineyardDao.addAll(List.of(testVineyard1, testVineyard2, testVineyard3));
 
   }
@@ -49,7 +52,7 @@ public class VineyardDaoTest {
   }
 
   @Test
-  void testGetCount() throws  SQLException {
+  void testGetCount() throws SQLException {
 
     int count = vineyardDao.getCount();
 
@@ -59,7 +62,7 @@ public class VineyardDaoTest {
   @Test
   void testGetAllInRange() throws SQLException {
 
-    int start = 0;
+    int start = 5;
     int end = 100;
 
     ObservableList<Vineyard> result = vineyardDao.getAllInRange(start, end, null);
@@ -67,11 +70,17 @@ public class VineyardDaoTest {
     assertNotNull(result);
 
     int size = result.size();
-    assertEquals(11, size); // 8 default plus 3 extra
+    assertEquals(6, size); // 8 default plus 3 extra
 
-    assertEquals(result.get(size-3).getName(), "Test1 Vineyard");
-    assertEquals(result.get(size-2).getName(), "Test2 Vineyard");
-    assertEquals(result.get(size-1).getName(), "Test3 Vineyard");
-
+    assertEquals(result.get(size - 3).getName(), "Test1 Vineyard");
+    assertEquals(result.get(size - 2).getName(), "Test2 Vineyard");
+    assertEquals(result.get(size - 1).getName(), "Test3 Vineyard");
   }
+
+  @Test
+  void testGetByName() {
+    Vineyard result = vineyardDao.get("Test1 Vineyard");
+  }
+
+
 }
