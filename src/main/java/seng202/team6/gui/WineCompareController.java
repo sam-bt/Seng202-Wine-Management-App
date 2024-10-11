fedd14082092d4af69f8a886e164c163c8efebe3
Corey Hines <chi125@uclive.ac.nz>
2024-10-11 13:22:58 +1300

    started on the layout for the wine compare screen

--- src/main/java/seng202/team6/gui/WineCompareController.java
+++ src/main/java/seng202/team6/gui/WineCompareController.java
@@ -1,10 +1,14 @@
 package seng202.team6.gui;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import seng202.team6.gui.controls.AutoCompletionTextField;
 import seng202.team6.managers.ManagerContext;
 import seng202.team6.model.Wine;
@@ -50,17 +54,23 @@ public class WineCompareController extends Controller {
 
     WineSide(VBox container) {
       this.container = container;
+      container.setPadding(new Insets(10));
+      container.setStyle("-fx-background-color: #f3f4f6; -fx-background-radius: 10px;");
       setup();
     }
 
-    void setup() {
+    private void setup() {
       searchTextField = new AutoCompletionTextField();
-      searchTextField.setMaxWidth(Double.MAX_VALUE);
+      searchTextField.setPrefWidth(200);
 
       Label searchLabel = new Label("Search for a Wine");
+      searchLabel.setFont(Font.font(18));
+
+      Separator separator = new Separator();
       HBox wrapper = new HBox(searchLabel, searchTextField);
-      HBox.setHgrow(searchTextField, Priority.ALWAYS);
-      container.getChildren().add(wrapper);
+      wrapper.setAlignment(Pos.CENTER);
+      wrapper.setSpacing(10);
+      container.getChildren().addAll(wrapper, separator);
     }
   }
 }
