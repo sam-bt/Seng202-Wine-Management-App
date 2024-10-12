ec69099c1e6338901b2da5b4d98c2d499ba0e533
amc510 <amc510@uclive.ac.nz>
2024-10-13 12:31:40 +1300

    Fix warn script

--- scripts/warn_javafx_version_in_fxml.sh
+++ scripts/warn_javafx_version_in_fxml.sh
@@ -4,7 +4,7 @@ cd ../src/main/resources/fxml || exit 1
 
 # Error if we dont find version 21 in the file
 # check 3 levels
-for fxml_file in *.fxml */*.fxml */*/*.fxml
+for fxml_file in *.fxml */*.fxml
 do
   grep -E -i "https?://javafx.com/javafx/21" $fxml_file
   if [[ $? -ne 0 ]]
