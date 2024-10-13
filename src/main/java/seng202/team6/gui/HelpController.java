f1fb4b4d57827c6930135f2c686c337d253ad941
mru50 <mru50@uclive.ac.nz>
2024-10-13 17:33:27 +1300

    Fixed warnings

--- src/main/java/seng202/team6/gui/HelpController.java
+++ src/main/java/seng202/team6/gui/HelpController.java
@@ -1,11 +1,9 @@
 package seng202.team6.gui;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import seng202.team6.managers.DatabaseManager;
 import seng202.team6.managers.ManagerContext;
 
 /**
@@ -28,19 +26,20 @@ public class HelpController extends Controller {
 
   @FXML
   private void initialize() {
+    String url = "";
     try {
-      String url = getClass().getResource("/html/ManualIndex.html").toExternalForm();
+      url = getClass().getResource("/html/ManualIndex.html").toExternalForm();
       if (managerContext.getAuthenticationManager().isAdmin()) {
         url = getClass().getResource("/html/AdminIndex.html").toExternalForm();
       }
 
-      webView.getEngine()
-          .setUserStyleSheetLocation(
-              getClass().getResource("/css/helppages.css").toExternalForm());
-      webView.getEngine().load(url);
     } catch (NullPointerException e) {
       log.info("Failed to load HTML index file");
       log.info(e.getMessage());
     }
+    webView.getEngine()
+        .setUserStyleSheetLocation(
+            getClass().getResource("/css/helppages.css").toExternalForm());
+    webView.getEngine().load(url);
   }
 }
